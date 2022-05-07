96e8f7fbe978673722c4bff46e21a1bf809cc3c4
Natalie Kwan <natalie@swuser-129-8-194-169.swlan.csufresno.edu>
2022-05-06 20:48:12 -0700

    finish pseudocode

--- test_2.py
+++ test_2.py
@@ -6,3 +6,23 @@ A text file called "Final.txt" has 24 student grades
 It is required to calculate the number of grades, the average grades and the grades that are above average
 Then print the outputs, "Number of Grades", "Average Grades", and "Percentage of grades above average".
 """
+
+
+"""
+infile = open("filename", 'r')
+L = [line.rstrip() for line in infile]
+infile.close
+input: student's grade
+output: number of grades, average grades, percentage of grades above average
+list of grades = lens(grades)
+sum = sum of all grades
+average = sum of grades / number of grades in list
+Percentage of grade above average = 100 * num / lens (grades)
+
+    if grade > average
+    "Percentage of grades above average: 100 * grade / lens (grades)"
+
+main
+"""
+
+
