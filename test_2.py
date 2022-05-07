9ee5dd912ec7216f63314833dc44f6dc8e41a3f3
Natalie Kwan <natalie@swuser-129-8-194-169.swlan.csufresno.edu>
2022-05-06 19:55:28 -0700

    finish structured english

---
+++ test_2.py
@@ -0,0 +1,8 @@
+
+
+"""
+The program is trying to show student grades from a final exam.
+A text file called "Final.txt" has 24 student grades
+It is required to calculate the number of grades, the average grades and the grades that are above average
+Then print the outputs, "Number of Grades", "Average Grades", and "Percentage of grades above average".
+"""
